dc0ecd2991954bf9c621e459d7b8e7239172cb7b
namcuong757 <69831060+namcuong757@users.noreply.github.com>
2021-10-17 14:44:48 -0700

    Update WriteToFileButtonListener.java

--- Java/Pay Calculator/src/WriteToFileButtonListener.java
+++ Java/Pay Calculator/src/WriteToFileButtonListener.java
@@ -1,14 +1,4 @@
-/**
- * Lead Author(s):
- * @author Cuong Tran
- * @author Nhat Le
- * 
- * Other contributors: none
- * 
- * Responsibilities of class: implementing ActionListener for WriteToFileButton from view Class
- * 
- * 
- */
+
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
